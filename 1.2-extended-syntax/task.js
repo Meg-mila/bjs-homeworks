05ddd1238e9ffb7d37a5deff64f1762722bc0a0f
Margarita Perepecho <meg.mila27@gmail.com>
2020-11-02 12:45:41 +0000

    add function getResult

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,6 +1,17 @@
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    "use strict"
+    let D = (Math.pow(b,2) - 4 * a * c); 
+    let x = [];
+    if (D < 0) {
+        return x;
+    } else if (D === 0) {
+        x = (-b / 2 * a);
+        return [x];
+    } else if (D > 0) {
+        let x1 = ((-b + Math.sqrt(D)) / 2 * a);
+        let x2 = ((-b - Math.sqrt(D)) / 2 * a);
+        return [x1, x2];
+    }
 }
 
 function getAverageMark(marks){
