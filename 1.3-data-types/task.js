80024b815d556bbc5aa4464166741a6e9e3cbdc4
Margarita Perepecho <meg.mila27@gmail.com>
2020-11-04 10:57:28 +0000

    completed 2 tasks

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,37 @@
+"use strict"
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+
+    if (isNaN(parseInt(percent))) {
+        return `Параметр Процентная ставка содержит неправильное значение ${percent}`;
+    }
+    if (isNaN(parseInt(contribution))) {
+        return `Параметр Первоначальный взнос содержит неправильное значение ${contribution}`;
+    }
+    if (isNaN(parseInt(amount))) {
+        return `Параметр Сумма кредита содержит неправильное значение ${amount}`;
+    } 
+    
+    let amountReturn = amount - contribution; // тело кредита
+
+    let month = date.getMonth() - new Date().getMonth();
+    if (date.getMonth() < new Date().getMonth()) {
+        month = new Date().getMonth() -date.getMonth();
+    }
+
+    let creditTerm = ((date.getFullYear() - new Date().getFullYear()) * 12) + month; // кол-во месяцев
+    let P = percent / 1200; // 1/12 процентной ставки
+
+    let amountMonth = amountReturn * (P + P / ((Math.pow((1 + P), creditTerm) - 1))); // ежемесячная оплата
+    let totalAmount = amountMonth * creditTerm;
+    console.log(totalAmount.toFixed(2));
+    return totalAmount.toFixed(2);
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    if (name === "") {
+        name = "Аноним";
+    }
+    let greeting = `Привет, мир! Меня зовут ${name}.`;
+    console.log(greeting);
+    return greeting;
 }
