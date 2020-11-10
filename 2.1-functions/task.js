cf7457e15e641e80ae763fe8a62b9e41368079ed
Margarita Perepecho <meg.mila27@gmail.com>
2020-11-10 15:02:13 +0000

    add function getAverageScore and

--- 2.1-functions/task.js
+++ 2.1-functions/task.js
@@ -1,30 +1,32 @@
 "use strict"
- // задача 1
+
+// задача 1
 function getSolutions(a, b, c) {
-    const D = (Math.pow(b, 2) - 4 * a * c);
-    if (D < 0) {
+    const d = (Math.pow(b, 2) - 4 * a * c);
+    if (d < 0) {
         return {
-            D: D,
+            D: d,
             roots: []
         };
-    } else if (D === 0) {
-        let x = -b / (2 * a);
+    } else if (d === 0) {
+        const x = -b / (2 * a);
         return {
-            D: D,
+            D: d,
             roots: [x]
         };
-    } else if (D > 0) {
-        let x1 = ((-b + Math.sqrt(D)) / (2 * a));
-        let x2 = ((-b - Math.sqrt(D)) / (2 * a));
+    } else if (d > 0) {
+        const x1 = ((-b + Math.sqrt(d)) / (2 * a));
+        const x2 = ((-b - Math.sqrt(d)) / (2 * a));
         console.log(x1, x2);
         return {
-            D: D,
+            D: d,
             roots: [x1, x2]
         };
     }
 }
-function showSolutionsMessage( a, b, c ) {
-    let result = getSolutions( a, b, c );
+
+function showSolutionsMessage(a, b, c) {
+    const result = getSolutions(a, b, c);
     console.log(`Вычисляем корни квадратного уравнения ${a}x^2 + ${b}x + ${c}`);
     console.log(`Значение дискриминанта: ${result.D}`);
     if (result.D < 0) {
@@ -35,3 +37,46 @@ function showSolutionsMessage( a, b, c ) {
         return `Уравнение имеет два корня. X₁ = ${result.roots[0]}, X₂ = ${result.roots[1]}`;
     }
 }
+
+//задача 2
+function getAverageMark(marks) {
+    let sumMarks = 0;
+    if (marks.length === 0) {
+        return 0;
+    }
+    for (let i = 0; i < marks.length; i++) {
+        sumMarks += marks[i];
+    }
+    let averageMark = sumMarks / marks.length;
+    return averageMark;
+}
+
+function getAverageScore(data) {
+    let result = {}
+    result.average = 0;
+    let sumSubject = 0;
+    let subjectLength = 0;
+    for (let subject in data) {
+        let value = data[subject];
+        result[subject] = getAverageMark(value);
+        sumSubject += result[subject];
+        subjectLength++;
+        result.average = sumSubject / subjectLength;
+    }
+    if (data === 0) {
+        return result.average = 0;
+    }
+    return result
+}
+
+console.log(getAverageScore({
+    algebra: [2, 4, 5, 2, 3, 4],
+        geometry: [2, 4, 5],
+        russian: [3, 3, 4, 5],
+        physics: [5, 5],
+        music: [2, 2, 6],
+        english: [4, 4, 3],
+        poetry: [5, 3, 4],
+        chemistry: [2],
+        french: [4, 4],
+}));
