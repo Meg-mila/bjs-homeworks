ba66285b6bfe48ecbdb92d626313f1cf5dd7c233
Margarita Perepecho <meg.mila27@gmail.com>
2020-11-12 19:57:38 +0000

    update task.js

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,12 +1,12 @@
 "use strict"
-
+//задача 1
 function isPalindrome(str) {
     str  = this.toLowerCase().replace(/\s/g,'');
     return str === str.split('').reverse().join('');
 }
 String.prototype.isPalindrome = isPalindrome;
 
-
+//задача 2
 function getAverageMark(marks) {
     let average = 0;
     if (marks.length === 0) {
@@ -20,22 +20,14 @@ function getAverageMark(marks) {
     let roundAverage = Math.round(average);
     return  roundAverage;
 }
-
+//задача 3
 function checkBirthday(userBirthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
     let today = new Date();
-    console.log(today);
     let now = today.getTime();
-    console.log(now);
     let userDate = new Date(userBirthday);
-    console.log(userDate);
     let birthday = userDate.getTime();
-    console.log(birthday);
     let diff = now - birthday;
-    console.log(diff);
     let age = diff / (24 * 3600 * 1000 * 365.2425);
-    console.log(age);
     if (age > 18) {
         return true
     } else if (age < 18) {
