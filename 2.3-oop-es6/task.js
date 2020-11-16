790bc84a2afbba395563e55c67453fccb55546ac
Margarita Perepecho <meg.mila27@gmail.com>
2020-11-16 13:06:50 +0000

    add task 1

---
+++ 2.3-oop-es6/task.js
@@ -0,0 +1,78 @@
+//задача 1
+const upState = 'fix';
+
+class PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.pagesCount = pagesCount;
+        this.state = 100;
+        this.type = null;
+    }
+    [upState]() {
+        this.state = this.state * 1.5
+    }
+
+    set stateNew(state) {
+        if (state < 0) {
+            this._state = 0;
+        } else if (state > 100) {
+            this._state = 100;
+        } else {
+            this._state = state;
+        }
+    }
+    get stateNew()
+    {
+        return this._state;
+    }
+}
+const sherlock = new PrintEditionItem("Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе", 2019, 1008);
+console.log(sherlock.releaseDate);
+console.log(sherlock.state);
+sherlock.fix();
+console.log(sherlock.state);
+
+class Magazine extends PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
+        this.type = 'magazine';
+    }
+}
+class Book extends PrintEditionItem {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
+        this.author = author;
+        this.type = 'book';
+    }
+}
+class NovelBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
+        this.type = 'novel';
+    }
+}
+class FantasticBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
+        this.type = 'fantastic';
+    }
+}
+class DetectiveBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
+        this.type = 'detective';
+    }
+}
+const picknick = new FantasticBook("Аркадий и Борис Стругацкие", "Пикник на обочине", 1972, 168);
+const pyshkin = new NovelBook('А.С.Пушкин', 'Руслан и Людмила', 1988, 150);
+console.log(picknick.author);
+picknick.state = 10;
+console.log(picknick.state);
+picknick.fix();
+console.log(picknick.state);
+console.log(pyshkin.author);
+pyshkin.state = 20;
+console.log(pyshkin.state);
+pyshkin.fix();
+console.log(pyshkin.state);
