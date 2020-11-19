217841e83515cd3c633fb5dacd093e8e2a4d7850
Margarita Perepecho <meg.mila27@gmail.com>
2020-11-19 19:25:58 +0000

    add task 2

--- 3.1-exceptions-closure/task.js
+++ 3.1-exceptions-closure/task.js
@@ -1,4 +1,5 @@
 'use strict'
+
 // Задача 1
 
 function parseCount(value) {
@@ -16,3 +17,44 @@ function validateCount(value) {
         return new Error("Невалидное значение");
     }
 }
+
+// Задача 2
+
+class Triangle {
+    constructor(a, b, c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        if (((a + b) < c) || ((a + c) < b) || ((b + c) < a)) {
+            throw new Error("Треугольник с такими сторонами не существует")
+        }
+    }
+
+    getPerimeter() {
+        return this.a + this.b + this.c;
+    }
+
+    getArea() {
+        const p = this.getPerimeter() / 2;
+        const s = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+        return +s.toFixed(3);
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        const triangle = new Triangle(a, b, c);
+        return triangle;
+    } catch {
+        return {
+            getArea() {
+                return 'Ошибка! Треугольник не существует';
+            },
+            getPerimeter() {
+                return 'Ошибка! Треугольник не существует';
+            }
+        }
+    }
+}
+
+
